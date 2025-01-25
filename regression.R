8952185fe229f2ffb23c6534f1b5db74b81f7425
Danbo-Chen219 <chen.12712@buckeyemail.osu.edu>
2025-01-25 11:21:51 -0500

    edit R script

--- regression.R
+++ regression.R
@@ -1,9 +1,11 @@
-rm(list=ls())
-{want <- c("stargazer", "car", "tidyverse","cowplot","clubsandwich","sandwich", "multiwayvcov","vtable" ) 
-  need <- want[!(want %in% installed.packages()[,"Package"])] 
-  if(length(need)) install.packages(need)
+rm(list = ls())
+{
+  want <- c("stargazer", "car", "tidyverse", "cowplot", "clubsandwich", "sandwich", "multiwayvcov", "vtable")
+  need <- want[!(want %in% installed.packages()[, "Package"])]
+  if (length(need)) install.packages(need)
   lapply(want, function(i) require(i, character.only = TRUE))
   rm(need)
-  }
-
+}
+install.packages('languageserver', repos = 'https://cloud.r-project.org/')
+library(languageserver)
 # Load data
