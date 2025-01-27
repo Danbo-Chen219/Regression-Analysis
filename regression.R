a7967ae933f70fe8ba4cfae28a2e3e1b17c3ec6f
Danbo-Chen219 <chen.12712@buckeyemail.osu.edu>
2025-01-26 19:53:00 -0500

    edit Rscript

--- regression.R
+++ regression.R
@@ -97,12 +97,13 @@ stargazer(ols, type = "text")
 
 getwd()
 dirname(getwd())
-install.packages("ggplot2", repos = "https://cloud.r-project.org/")
-install.packages("cowplot", repos = "https://cloud.r-project.org/")
+# install.packages("ggplot2", repos = "https://cloud.r-project.org/")
+install.packages("knitr", repos = "https://cloud.r-project.org/")
 library(dplyr)
 library(ggplot2)
 library(cowplot)
 library(stargazer)
+library(knitr)
 # working directory where datasets are saved
 wagedata <- read.csv("wage2.csv")
 # loading wage data
@@ -148,3 +149,214 @@ ols1 <- lm(logwage ~ educ + exper + exper2, data = wagedata)
 ols2 <- lm(logwage ~ educ + exper + exper2 + tenure + married + black + south + urban, data = wagedata)
 
 stargazer(ols1, ols2, type = "text", title = "Wage Regression Results")
+### get standard error of each coefficients
+
+## 1) using vcov
+
+coefs <- summary(ols2)$coefficients
+se <- sqrt(diag(vcov(ols2)))
+# Getting OLS coefficients and their standard errors from the second model
+
+plot_order <- c("educ", "exper", "exper2", "tenure", "married", "black", "south", "urban")
+
+plot <- data.frame(
+  se = c(summary(ols2)$coefficients[, 2][plot_order]),
+  mean = c(coef(ols2)[plot_order]),
+  label = c("educ", "exper", "exper2", "tenure", "married", "black", "south", "urban")
+)
+
+plot %>%
+  ggplot(aes(
+    x = label, y = mean,
+    ymin = mean - 1.96 * se,
+    ymax = mean + 1.96 * se
+  )) +
+  geom_hline(yintercept = 0, color = "black") +
+  geom_pointrange() +
+  theme_minimal() +
+  xlab("Variable name") +
+  ylab("Parameter estimate")
+
+# bootstrapping the education coefficient
+
+m <- 1000 # of bootstraps;
+
+bhat_educ <- rep(0, m)
+for (i in 1:m) {
+  random_sample <- wagedata[sample(nrow(wagedata), size = nrow(wagedata), replace = TRUE), ]
+  ols_random_sample <- lm(logwage ~ educ + exper + exper2 + tenure + married + black + south + urban, data = random_sample)
+  bhat_educ[i] <- coefficients(ols_random_sample)[2]
+  print(paste(i, "loop finished"))
+}
+
+
+hist(bhat_educ, breaks = 50, freq = FALSE)
+# remove freq=FALSE to obtain counts/frequencies as opposed to probabilities. More breaks to obtain a finer grid.
+
+x <- bhat_educ
+# rename the variable to be plotted as x because that's the default in the curve function below
+
+mean_x <- mean(bhat_educ)
+sd_x <- sd(bhat_educ)
+curve(dnorm(x, mean = mean_x, sd = sd_x), add = TRUE, col = "blue")
+
+
+quantile(bhat_educ, prob = 0.025)
+quantile(bhat_educ, prob = 0.975)
+# finding the 5% two-sided confidence interval by the percentile bootstrap method
+# 95% confident that the true coefficient on education lies between the 2.5 and 97.5th percentiles
+
+
+
+#----------------------------Code # 3-------------------------------------------
+
+rm(list = ls())
+# install.packages("jtools", repos = "https://cloud.r-project.org/")
+
+# install.packages("fpc", repos = "https://cloud.r-project.org/")
+library(fpc)
+library(sandwich)
+library(skimr)
+# ls("package:skimr")
+library(gtsummary)
+library(knitr)
+library(dplyr)
+library(ggplot2)
+library(cowplot)
+library(stargazer)
+library(jtools)
+library(stringr)
+uganda <- read.csv("uganda.csv")
+
+uganda <- mutate(uganda, logfarmrev = log(sfarmrev))
+
+uganda <- uganda %>%
+  mutate(logfarmrev = log(sfarmrev))
+
+# computes and adds the log of farm revenue
+
+uganda_sub <- dplyr::select(
+  uganda, logfarmrev, farmsize, hhsize,
+  extvisits, agehead, hhavgeduc,
+  fertilizer, seedtechs, malehead,
+  distance, drought, urban,
+  year, hhid, comm
+)
+
+stargazer(uganda_sub %>% data.frame(), type = "text") # outputs a summary of data
+# Summary statistics of the data
+
+# Doing the same commands with pipes (%.%) which allow the sequencing  of multiple operations.
+
+uganda %>%
+  data.frame() %>%
+  mutate(logfarmrev = log(sfarmrev)) %>%
+  dplyr::select(
+    logfarmrev, farmsize, hhsize, extvisits, agehead, hhavgeduc,
+    fertilizer, seedtechs, malehead, distance, drought, urban,
+    year, hhid, comm
+  ) %>%
+  stargazer(type = "text")
+# Computes and adds the log of farm revenue and selects a subset of variables
+# summary statistics of the data are added
+# Without the data.frame command, the code was generating the following an error message:
+# Error in UseMethod("mutate") :
+# no applicable method for 'mutate' applied to an object of class "character"
+
+# uganda_sub %>%
+#   dplyr::select(-year, -hhid, -comm) %>%
+#   sumtable(group = "fertilizer", group.test = TRUE, file = "balance", out = "viewer")
+# # Balance test (difference of means) by fertilizer use, after eliminating variables year, hhid and comm
+
+# uganda_sub %>%
+#   select(-year, -hhid, -comm) %>%
+#   group_by(fertilizer) %>%
+#   summarise(mean_value = mean(value, na.rm = TRUE)) %>%
+#   kable()
+
+# OLS estimation
+ols1 <- lm(logfarmrev ~ fertilizer, data = uganda)
+
+
+# OLS estimation
+ols2 <- lm(logfarmrev ~ fertilizer + farmsize + hhsize + extvisits + agehead + hhavgeduc +
+  seedtechs + malehead + distance + drought + urban, data = uganda)
+
+
+# OLS estimation
+ols3 <- lm(logfarmrev ~ fertilizer + farmsize + hhsize + extvisits + agehead + hhavgeduc +
+  seedtechs + malehead + distance + drought + urban + factor(comm), data = uganda)
+
+stargazer(ols1, ols2, ols3,
+  type = "text",
+  # Do not report community FE
+  omit = "comm",
+  # Omit residual SE and R2
+  omit.stat = c("ser", "rsq"),
+  add.lines = list(c("Community Fixed effects", "No", "No", "Yes")),
+  notes = "Assume homoscedasticity.",
+  column.labels = str_c("Model", rep(1:3, 3)),
+  # Dependent variable label
+  dep.var.caption = "\\textit{Dependent variable:} Log of Farm Revenue",
+  dep.var.labels = NULL,
+  dep.var.labels.include = F,
+  title = "Wage Regression Results"
+)
+
+
+### Computing robust standard errors based on Model 2 ###
+
+count(uganda) # number of observations
+length(unique(uganda$comm))
+# number of communities (clusters). Minimum of 30 is required for clustering
+
+uganda_sub %>%
+  group_by(comm) %>%
+  count()
+# Reports the number of observations per community
+
+# statements require the dplyr library
+
+
+# Variance matrices
+OLS <- vcov(ols2) # ordinary
+
+HC <- vcovHC(ols2, type = "HC1") # Heteroscedasticity-consistent (HC)
+# HC0 gives White's robust errors. HC1 applies a small sample adjustment
+# HC1 applies a degrees of freedom-based correction: (n-1/n-k)
+
+CR <- vcov(ols2, type = "CR1", cluster = uganda$comm) # Cluster-robust
+# See pdf file titled Package clubSandwich July 20, 2023 for more information on options
+
+IB <- cluster.boot(ols2, cluster = 1:nrow(uganda), R = 100) # Individual data bootstrap
+
+# First, transform the cluster variable to factor and transform to numeric or integer
+# for cluster bootstrap to work. No sure why that's the case.
+uganda$comm <- as.numeric(as.factor(uganda$comm))
+
+CB <- cluster.boot(ols2, cluster = uganda$comm, R = 100) # Cluster bootstrap
+
+# getting standard errors
+std.error <- list(OLS, HC, CR, IB, CB) %>% # list of VCOV matrices
+  map(~ sqrt(diag(.))) %>% # extract diagonals and find their square roots
+  reduce(data.frame) %>% # reduce a list to a data.frame
+  setNames(c("Unadjusted", "HC", "CL", "IB", "CB")) # set column names
+
+
+# Table of results saved in working directory
+stargazer(rep(list(ols2), 5),
+  se = std.error, type = "text", no.space = T, df = F,
+  column.labels = c("Unadjusted", "HC", "CR", "IB", "CB"),
+  title = "Determinants of Farm Income in Uganda",
+  out = "table1.txt"
+)
+
+stargazer(rep(list(ols2), 5),
+  se = std.error, type = "text", no.space = T, df = F,
+  column.labels = c("Unadjusted", "HC", "CR", "IB", "CB"),
+  title = "Determinants of Farm Income in Uganda",
+  out = "table1.html"
+)
+
+dev.off()
+# clears plots
